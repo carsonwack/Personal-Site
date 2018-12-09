8b8c52a79480328fcb88f70cba6dcc909cdb5500
Carson Wack <carson_wack@yahoo.com>
2018-12-08 21:30:43 -0800

    not done yet but mostly functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,36 @@
 $(document).ready(function () {
 
+    $('#fallacy').click(function () {
+        window.location = "https://carsonwack.github.io/TriviaGame/";
+    });
+
+    $('#stopwatch').click(function () {
+        window.location = "https://carsonwack.github.io/stopwatch/";
+    });
+
+    $('#gifs').click(function () {
+        window.location = "https://carsonwack.github.io/GiphyAPI--Comedians/";
+    });
+
+    $('#train').click(function () {
+        window.location = "https://carsonwack.github.io/TrainTrack/";
+    });
+
+    $('#betkeep').click(function () {
+        window.location = "https://carsonwack.github.io/BetKeeper/";
+    });
+
+    $('#ingredient-list').click(function () {
+        window.location = "https://curtisyungen.github.io/project-one/";
+    });
+
+    $('#git-link').click(function () {
+        window.location = "https://github.com/carsonwack";
+    });
+
+    $('#linked-link').click(function () {
+        window.location = "https://www.linkedin.com/in/carson-wack-206/";
+    });
 
 
 });
