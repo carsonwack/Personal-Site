1515503aa74db0eea8ab13ee154af142edefa95a
Carson Wack <carson_wack@yahoo.com>
2018-12-09 11:53:45 -0800

    navbar highlighting while tracking sroll

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,5 +32,31 @@ $(document).ready(function () {
         window.location = "https://www.linkedin.com/in/carson-wack-206/";
     });
 
+    // _________________________________________________________________
+
+
+    var $sections = $('.scrollable');
+
+    $(window).scroll(function() {
+        var currentScroll = $(this).scrollTop();
+
+        var $currentSection;
+
+        $sections.each(function () {
+
+            var divPosition = $(this).offset().top;
+
+            if(divPosition - 1 < currentScroll ) {
+                $currentSection = $(this);
+            }
+
+            var id = $currentSection.attr('id');
+            $('a').removeClass('active');
+            $("[href='#"+id+"']").addClass('active');
+
+        })
+    });
+
+
 
 });
